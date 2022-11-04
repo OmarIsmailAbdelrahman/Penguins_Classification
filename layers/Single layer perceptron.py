927205543313b00196501414d901da2b62dc649d
Omar Ismail <omar_som3a2015@hotmail.com>
2022-11-04 15:43:10 +0200

    update train function and adding more function to the SingleLayer Class

--- layers/Single layer perceptron.py
+++ layers/Single layer perceptron.py
@@ -8,10 +8,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
 
 np.set_printoptions(threshold=sys.maxsize)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
+
+def feature_scaling(x):
+    """ Standardisation """
+    standardisation = preprocessing.StandardScaler()
+    # Scaled feature
+    x_after_standardisation = standardisation.fit_transform(x)
+    return x_after_standardisation
+
+def fun(x):
+    if x < 0:
+        return -1
+    else:
+        return 1
+
 
 class SingleLayer:
     def __init__(self, alpha=0.01,max_iter=1000,reg_constant = 0.001,bias= True):
@@ -21,38 +37,48 @@ class SingleLayer:
         self.bias = bias
         self.max_iter = max_iter
     def threshold(self,x):
-        return 1 / (1 + np.exp(-x))
-
-    def dthreshold(self, x):
-        return self.sigmoid(x) * (1 - self.sigmoid(x))
+        return np.apply_along_axis(fun, 1, x).reshape(-1,1)
 
     def loss(self, y, y_hat):
-        return 1 / 2 * (y - y_hat) ** 2
+        return np.array(1 / 2 * (y - y_hat) ** 2)
 
     def dloss(self, y, y_hat):
         return (y - y_hat)
-
+    def predict(self,X):
+        return np.dot(X,self.W)
+    def differnce(self,y,y_hat):
+        x = 0
+        for i in range(y.shape[0]):
+            if y[i] != y_hat[i]:
+                x+=1
+        return  1 - np.abs(x/y.shape[0])
     def train(self,X,y):
+        # initialization of weights
+        y = y-1
+        print(y.reshape(1,-1))
         if self.bias:
             X = np.hstack((np.ones([X.shape[0], 1]), X))
             num = 3
         else:
+            X = np.array(X)
             num = 2
-        np.random.seed(1234)
-        W = (np.random.rand(num))
+        # np.random.seed(1234)
+        self.W = (np.random.rand(num)).reshape(-1,1)
         if self.bias:
-            W[0] = 1
+            self.W[0] = 1
         for i in range(self.max_iter):
-            Z = np.dot(X,W)
+            Z = np.dot(X,self.W)
             A = self.threshold(Z)
-            error = self.loss(A,y)
-            print(error)
-
+            error = self.dloss(A,y)
+            self.W = self.W - self.alpha * np.dot(X.T,error) / X.shape[0]
+            if(self.differnce(A,y) > 0.95):
+                print(i)
+                break
 
 df = pd.read_csv('penguins.csv', index_col=False,encoding="utf-8")
 df.reset_index(drop=True, inplace=True)
 
-print(df.isna().sum())
+# print(df.isna().sum())
 # only 6 missing values
 le = LabelEncoder()
 df["gender"] = df.apply(le.fit_transform)["gender"]
@@ -71,15 +97,14 @@ plt.scatter(df.bill_length_mm, df.flipper_length_mm, c=df.gender)
 
 n = 100
 bias = True
-
-if bias:
-    X = np.random.rand(n,3)
-else:
-    X = np.random.rand(n, 2)
+# if bias:
+#     X = np.random.rand(n,3)
+# else:
+#     X = np.random.rand(n, 2)
 y = np.random.rand(n)
-model = SingleLayer(bias=bias,max_iter = 1,alpha = 1000)
-## do: make the y take only two classes
-model.train(df[["bill_length_mm","bill_depth_mm"]],df["species"],)
+model = SingleLayer(bias=bias,max_iter = 1000,alpha =0.01)
+X = feature_scaling(df[["bill_length_mm","bill_depth_mm"]])[:100]
+# if 1 and 2 class make it 0 and 1 or something
+model.train(X,np.array(df["species"]).reshape(-1,1)[:100],)
 
 
-# bill_length_mm  bill_depth_mm  flipper_length_mm
